f2bde6ae51fc231888454383e0efc54abb7c26b6
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:09:33 -0400

    Add approval gate kit folder

---
+++ kits/approval-gate-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createApprovalGateDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("approval-gate-domain-kit");
