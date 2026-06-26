a16b715c352c7bbca826f2e266944549a1d324fa
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:06:38 -0400

    Add deployment plan kit folder

---
+++ kits/deployment-plan-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createDeploymentPlanDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("deployment-plan-domain-kit");
