cfed65272da511b02ac9c4446316f5fbb02542f5
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:10:40 -0400

    Add rollback snapshot kit folder

---
+++ kits/rollback-snapshot-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createRollbackSnapshotDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("rollback-snapshot-domain-kit");
