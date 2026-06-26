e59215189c758080855173a4825af5a4c1155f56
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:07:53 -0400

    Add command dispatch kit folder

---
+++ kits/command-dispatch-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createCommandDispatchDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("command-dispatch-domain-kit");
