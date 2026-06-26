f8d67e3a3dab7f47236aa625bc97cb18c0df3118
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:04:17 -0400

    Add fleet registry kit folder

---
+++ kits/fleet-registry-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createFleetRegistryDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("fleet-registry-domain-kit");
