ba3de97c097847b58ef90d7307c23c3dddd6cbb6
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:07:19 -0400

    Add rollout orchestration kit folder

---
+++ kits/rollout-orchestration-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createRolloutOrchestrationDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("rollout-orchestration-domain-kit");
