deedac564a28e78eca77a951a6dbce1c90d76454
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:11:52 -0400

    Add fleet sequence kit folder

---
+++ kits/fleet-sequence-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createFleetSequenceDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("fleet-sequence-domain-kit");
