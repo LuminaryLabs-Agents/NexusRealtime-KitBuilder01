0c923c4d385db128ab47e285cc0b94d72dda5b93
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:08:59 -0400

    Add policy compliance kit folder

---
+++ kits/policy-compliance-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createPolicyComplianceDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("policy-compliance-domain-kit");
