21368f83f171033579df0730bbc01b33e222a6f3
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:06:19 -0400

    Add install state kit folder

---
+++ kits/install-state-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createInstallStateDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("install-state-domain-kit");
