d288028425c275d5f3036d1b22f80976792858ec
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:05:44 -0400

    Add package catalog kit folder

---
+++ kits/package-catalog-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createPackageCatalogDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("package-catalog-domain-kit");
