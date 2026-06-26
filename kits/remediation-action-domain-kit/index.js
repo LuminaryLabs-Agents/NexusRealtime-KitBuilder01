33e75b6b375fbf2b9bd44f59e3e4e4ff64c3b3bd
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:10:59 -0400

    Add remediation action kit folder

---
+++ kits/remediation-action-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createRemediationActionDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("remediation-action-domain-kit");
