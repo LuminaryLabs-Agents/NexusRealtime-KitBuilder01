7f9f29a37616d8050adb93aff47327ed95f4bd4a
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:05:23 -0400

    Add device health kit folder

---
+++ kits/device-health-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createDeviceHealthDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("device-health-domain-kit");
