d652c56321fc74f639f7a887a2f11bd34196741d
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:04:51 -0400

    Add device identity kit folder

---
+++ kits/device-identity-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createDeviceIdentityDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("device-identity-domain-kit");
