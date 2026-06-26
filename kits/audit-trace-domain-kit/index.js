522fef4755ec24420c12b05442d96407f26b7904
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:10:06 -0400

    Add audit trace kit folder

---
+++ kits/audit-trace-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createAuditTraceDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("audit-trace-domain-kit");
