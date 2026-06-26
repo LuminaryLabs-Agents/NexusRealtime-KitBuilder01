9ca2e1b315060cf63c2322347dd14518e1fb58e3
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-25 23:11:19 -0400

    Add dashboard view kit folder

---
+++ kits/dashboard-view-domain-kit/index.js
@@ -0,0 +1,4 @@
+import { getSpeedDeployKitManifest } from "../manifests.js";
+export { createDashboardViewDomainKit } from "../index.js";
+export const VERSION = "0.1.0";
+export const manifest = getSpeedDeployKitManifest("dashboard-view-domain-kit");
